17278f5fca72bc592caafe4077424a91686778dd
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2018-09-05 16:45:30 -0700

    new edit

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,16 +1,22 @@
-require 'securerandom'
-
 class ShortenedUrl < ApplicationRecord
-  
+
   def self.random_code
     url = "nick.com/="
     potential = ""
-    loop do 
+    loop do
       long_portion = SecureRandom.base64
       potential = url + long_portion
-      
+
       break unless self.exists?(:short_url => potential)
     end
     potential
+  end
+
+  def self.short_url_creator(user_id , the_long_url)
+    self.create!(
+      user_id:  user_id ,
+      :long_url => the_long_url,
+      :short_url => ShortenedUrl.random_code
+    )
   end
 end
